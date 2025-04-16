91c6b789788dd9e089648596d90cd0c208688071
therealadik <vladerm2000@yandex.ru>
2025-04-16 22:46:44 +0500

    fix jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.4"
-	id("io.spring.dependency-management") version "1.1.7"
-	jacoco
+    java
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    jacoco
 }
 
 group = "com.example"
@@ -11,99 +11,97 @@ version = "1.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	/**
-	 * Spring boot starters
-	 */
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    /**
+     * Spring boot starters
+     */
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
-	/**
-	 * JWT
-	 */
-	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    /**
+     * JWT
+     */
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
-	/**
-	 * Swagger / OpenAPI
-	 */
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+    /**
+     * Swagger / OpenAPI
+     */
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 
-	/**
-	 * Database
-	 */
-	implementation("org.liquibase:liquibase-core")
-	implementation("redis.clients:jedis:5.2.0")
-	runtimeOnly("org.postgresql:postgresql")
+    /**
+     * Database
+     */
+    implementation("org.liquibase:liquibase-core")
+    implementation("redis.clients:jedis:5.2.0")
+    runtimeOnly("org.postgresql:postgresql")
 
-	/**
-	 * Utils & Logging
-	 */
-	implementation("com.fasterxml.jackson.core:jackson-databind")
-	implementation("org.slf4j:slf4j-api")
-	implementation("ch.qos.logback:logback-classic")
-	implementation("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	implementation("org.mapstruct:mapstruct:1.6.3")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    /**
+     * Utils & Logging
+     */
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("org.slf4j:slf4j-api")
+    implementation("ch.qos.logback:logback-classic")
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
-	/**
-	 * Tests
-	 */
-	testImplementation("org.junit.jupiter:junit-jupiter-params")
-	testImplementation("org.assertj:assertj-core")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    /**
+     * Tests
+     */
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	/**
-	 * Test containers
-	 */
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
+    /**
+     * Test containers
+     */
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
 }
 
 tasks.test {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 val test by tasks.getting(Test::class) { testLogging.showStandardStreams = true }
 
 tasks.bootJar {
-	archiveFileName.set("service.jar")
+    archiveFileName.set("service.jar")
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
-	reports {
-		html.required.set(true)
-		xml.required.set(true)
-	}
+    dependsOn(tasks.test)
+    reports {
+        html.required = true
+    }
 }
 
 tasks.jacocoTestCoverageVerification {
-	dependsOn(tasks.jacocoTestReport)
-	violationRules {
-		rule {
-			element = "PACKAGE"
-			includes = listOf("com.example.globus.service")
+    dependsOn(tasks.jacocoTestReport)
+    violationRules {
+        rule {
+            element = "PACKAGE"
+            includes = listOf(
+                "com.example.globus.service.*",
+                "com.example.globus.security"
+            )
 
-			limit {
-				minimum = BigDecimal.valueOf(0.7)
-			}
-		}
-	}
-}
-
-tasks.check {
-	dependsOn(tasks.jacocoTestCoverageVerification)
+            limit {
+                minimum = BigDecimal.valueOf(0.7)
+            }
+        }
+    }
 }

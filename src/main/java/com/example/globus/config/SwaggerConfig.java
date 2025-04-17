5b61a34de32c2ba4c43c02589724f8db66fd8fce
Александр(MasterDracula) <piskstik@gmail.com>
2025-04-17 23:37:42 +0300

    Add swager code v1.0, add dependences, rewrite clasess

--- src/main/java/com/example/globus/config/SwaggerConfig.java
+++ src/main/java/com/example/globus/config/SwaggerConfig.java
@@ -1,26 +1,58 @@
 package com.example.globus.config;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import static jdk.internal.joptsimple.util.RegexMatcher.regex;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Value("${swagger.enable:true}")
+    private boolean enableSwagger;
+
     @Bean
     public Docket productApi() {
-        /*создание контейнера для API окументации*/
-        return new Docket (DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.globus.controller"))
-                .paths(regex("/")) /*десь должно быть название эндпоинтов*/
+                .paths(PathSelectors.any()) // Включаем все эндпоинты
                 .build()
-                .apiInfo(metaData);
-    }
-    private ApiInfo metaData(){
-        return ApiInfoBuilder()
-                .title("")
-                .description("\"financial monitoring and reporting API")
+                .apiInfo(metaData())
+                .enable(enableSwagger); // Возможность отключения Swagger через конфиг
     }
 
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Financial Monitoring and Reporting API")
+                .description("API для финансового мониторинга и отчетности")
+                .version("1.0.0")
+                .contact(new Contact(/*тут наши данные для апи адрес, имя и т.д.*/
+                ))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }

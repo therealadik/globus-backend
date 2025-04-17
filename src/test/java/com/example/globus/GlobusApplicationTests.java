963fde862ac9db0499981009e3ff948e07aa09a6
therealadik <vladerm2000@yandex.ru>
2025-04-17 10:38:35 +0500

    add context tests

--- src/test/java/com/example/globus/GlobusApplicationTests.java
+++ src/test/java/com/example/globus/GlobusApplicationTests.java
@@ -2,8 +2,29 @@ package com.example.globus;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@SpringBootTest
 class GlobusApplicationTests {
+
+	@Container
+	static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:17.2")
+			.withDatabaseName("postgres")
+			.withUsername("user")
+			.withPassword("password");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+	}
 
 	@Test
 	void contextLoads() {

5ab08ed3c0b98e6514afe27e827504f823396526
therealadik <vladerm2000@yandex.ru>
2025-04-22 12:46:58 +0500

    init logic

--- src/main/java/com/example/globus/service/DashboardService.java
+++ src/main/java/com/example/globus/service/DashboardService.java
@@ -1,8 +1,11 @@
 package com.example.globus.service;
 
 import com.example.globus.dto.dashboard.DashboardTransactionCountDto;
+import com.example.globus.dto.dashboard.DebitCreditTransactionsDto;
 import com.example.globus.entity.transaction.Transaction;
 import com.example.globus.entity.transaction.TransactionStatus;
+import com.example.globus.entity.transaction.TransactionType;
+import com.example.globus.mapstruct.TransactionMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,8 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class DashboardService {
+
+    private final TransactionMapper transactionMapper;
 
     /**
      * Подсчитывает количество проведённых и отменённых транзакций.
@@ -27,5 +32,19 @@ public class DashboardService {
                 .count();
         long completed = transactions.size() - canceled;
         return new DashboardTransactionCountDto((int) completed, (int) canceled);
+    }
+
+    public DebitCreditTransactionsDto calculateDebitCreditTransactions(List<Transaction> transactions) {
+        var creditTransactions = transactions.stream()
+                .filter(t -> t.getTransactionType().equals(TransactionType.INCOME))
+                .map(transactionMapper::toDto)
+                .toList();
+
+        var debitTransactions = transactions.stream()
+                .filter(t -> t.getTransactionType().equals(TransactionType.EXPENSE))
+                .map(transactionMapper::toDto)
+                .toList();
+
+        return new DebitCreditTransactionsDto(debitTransactions, creditTransactions);
     }
 }
